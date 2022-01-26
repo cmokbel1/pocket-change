b591430eae50bb22330eb8481e8ceab36f36e8fd
timduran <durantimothy21@gmail.com>
2022-01-26 11:22:29 -0800

    added updated to user,js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const { Schema, model } = require('mongoose')
 
 const User = new Schema({
   username: String,
-  email: String,
-  
-}, 
+  email: String
+
+})
 
 User.plugin(require('passport-local-mongoose'))
 
