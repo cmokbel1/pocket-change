edd37d0d6808fc73abb922264dae71d7565bb04f
Buff4eyes <claudemokbel@gmail.com>
2022-01-26 11:03:50 -0800

    renamed expenses to actual

--- routes/actualRoutes.js
+++ routes/actualRoutes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router()
-const { Song, User } = require('../models')
+const { Actual, User } = require('../models')
 const passport = require('passport')
 
-router.get('/songs', passport.authenticate('jwt'), async function (req, res) {
+router.get('/actuals', passport.authenticate('jwt'), async function (req, res) {
   const songs = await Song.find({})
   res.json(songs)
 })
 
-router.post('/songs', passport.authenticate('jwt'), async function (req, res) {
-  const song = await Song.create(req.body)
-  await User.findByIdAndUpdate(req.user._id, { $push: { songs: song._id } })
+router.post('/actuals', passport.authenticate('jwt'), async function (req, res) {
+  const song = await Actual.create(req.body)
+  await User.findByIdAndUpdate(req.user._id, { $push: { actuals: actuals._id } })
   res.json(song)
 })
 
-router.put('/songs/:id', passport.authenticate('jwt'), async function (req, res) {
+router.put('/actuals/:id', passport.authenticate('jwt'), async function (req, res) {
   await Song.findByIdAndUpdate(req.params.id, req.body)
   res.sendStatus(200)
 })
