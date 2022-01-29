c24e3064556627528c2a26c983180e63cf50ade3
Buff4eyes <claudemokbel@gmail.com>
2022-01-28 19:01:44 -0800

    negated need for expensecontext, and form

--- client/src/pages/Budget/Budget.js
+++ client/src/pages/Budget/Budget.js
@@ -2,43 +2,58 @@ import Navbar from '../../components/NavBar'
 import { FormControl, InputLabel, Input, Button, FormHelperText } from '@mui/material'
 import ExpenseCard from '../../components/ExpenseCard'
 import axios from 'axios'
-import React from 'react'
-import  useState  from 'react'
+import { React, useState } from 'react'
+import { Grid } from '@mui/material'
+import { categoryResult, calcSumTotal } from '../../utils/CategoryResult'
 
 const Budget = () => {
-  const [
-    categoriesToAdd,
-    setCategoriesToAdd,
-  ] = React.useState(1);
-  const [
-    committedCategoriesToAdd,
-    setCommittedCategoriesToAdd,
-  ] = React.useState(0);
+  const [ categoriesToAdd, setCategoriesToAdd ] = useState(0);
+  const [ committedCategoriesToAdd, setCommittedCategoriesToAdd] = useState(0);
+  
+  const [ expenseState, setExpenseState ] = useState({ 
+    category: '',
+    goalValue: 0,
+    actualValue: 0,
+    handleAddExpense: (actualValue, goalValue) => {
+      return categoryResult(actualValue, goalValue)
+    },
+    handleInputChange: ({ target: { name, value } }) => setExpenseState({ ...expenseState, [name]: value }), 
+  })
 
-  const addExpense = () => {
-    console.log('yepp')
-  }
+
 
   const CategoryForm = () => [
+  
+
     <FormControl>
-      <Input name="category" aria-describedby="expense category" />
-      <FormHelperText id="my-helper-text">expense category</FormHelperText>
-      <Input type="number" name="actualValue" aria-describedby="actual value" />
-      <FormHelperText id="my-helper-text">actual expense</FormHelperText>
-      <Input type="number" name="goalValue" aria-describedby="goal value" />
-      <FormHelperText id="my-helper-text">goal expense</FormHelperText>
-      <Button onClick={addExpense}>Add</Button>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1 }}>
+        <Grid item xs={2}>
+          <Input name="category" aria-describedby="expense category" value={expenseState.category} onChange={expenseState.handleInputChange}/>
+          <FormHelperText id="my-helper-text">expense category</FormHelperText>
+        </Grid>
+        <Grid item xs={2}>
+          <Input type="number" name="actualValue" aria-describedby="actual value" value={expenseState.actualValue} onChange={expenseState.handleInputChange}/>
+          <FormHelperText id="my-helper-text">actual expense</FormHelperText>
+        </Grid>
+        <Grid item xs={2}>
+          <Input type="number" name="goalValue" aria-describedby="goal value" value={expenseState.goalValue} onChange={expenseState.handleInputChange}/>
+          <FormHelperText id="my-helper-text">goal expense</FormHelperText>
+        </Grid>
+        <Grid item xs={2}>
+          <Button onClick={expenseState.handleAddExpense}>Add</Button>
+        </Grid>
+      </Grid>
     </FormControl>
   ]
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <hr />
       <FormControl>
         <InputLabel htmlFor="numCategories">Categories</InputLabel>
         <Input name="categories" value={categoriesToAdd} type="number" aria-describedby="number of categories" onChange={(e) =>
           setCategoriesToAdd(
-            parseInt(e.currentTarget.value, 10))} />
+            parseInt(e.currentTarget.value))} />
         <FormHelperText id="my-helper-text">How Many Categories Would You Like to Use</FormHelperText>
         <Button
           onClick={() => {
@@ -48,13 +63,12 @@ const Budget = () => {
           }}
         >Confirm</Button>
       </FormControl>
-      <div className="container">
-        {[...Array(committedCategoriesToAdd)].map((value: undefined, index: number) => (
+      {[...Array(committedCategoriesToAdd)].map((value: undefined, index: number) => (
         <CategoryForm id={index + 1} key={index} />))}
-      </div>
       <h1>This is the Budget Page</h1>
     </>
   )
 }
 
 export default Budget
+
