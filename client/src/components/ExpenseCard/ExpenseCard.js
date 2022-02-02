4f736765ad0c1a7f71387192344386b4a2db8317
Buff4eyes <claudemokbel@gmail.com>
2022-02-02 15:05:07 -0800

    expensecard isthe issue

--- client/src/components/ExpenseCard/ExpenseCard.js
+++ client/src/components/ExpenseCard/ExpenseCard.js
@@ -1,34 +1,29 @@
 import { Grid, Button, Input, TextField } from '@mui/material'
 import axios from 'axios'
-import { json } from 'express/lib/response'
-import { useState } from 'react'
+import {useState} from 'react'
 
 
 const ExpenseCard = ({ category, goalValue, actualValue, result, _id }) => {
   const [cardState, setCardState] = useState(true)
   const [editState, setEditState] = useState({
-    actual: actualValue,
-    goal: goalValue,
-    result: result
+    actual: '',
+    goal: ''
   })
   const handleRemoveExpense = (id) => {
-    axios.delete(`/api/categories/${id}`, {
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('user')}`
-      }
-    }).then(res => {
-      console.log(res)
-      setCardState(false)
-    })
-
-  }
-  const handleEdit = (id) => {
-    let updatedExpense = {
-      actualValue: editState.actual,
-      goalValue: editState.goal,
-      result: JSON.parse(editState.goal - editState.actual)
+  axios.delete(`/api/categories/${id}`, {
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('user')}`
     }
-    axios.put(`/api/categories/${id}`, updatedExpense, {
+  }).then(res => { console.log(res) 
+    setCardState(false)
+  })
+
+} 
+  const handleEditActual = (id) => {
+    let updatedActual = {
+      actualValue: editState.actual
+    }
+    axios.put(`/api/categories/${id}`, updatedActual, {
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('user')}`
       }
@@ -36,37 +31,37 @@ const ExpenseCard = ({ category, goalValue, actualValue, result, _id }) => {
       console.log(res)
     })
 
-  }
-  // const handleEditGoal = (id) => {
-  //   let updatedGoal = {
-  //     goalValue: editState.goal
-  //   }
-  //   axios.put(`/api/categories/${id}`, updatedGoal, {
-  //     headers: {
-  //       'Authorization': `Bearer ${localStorage.getItem('user')}`
-  //     }
-  //   }).then(res => {
-  //     console.log(res)
-  //   })
+  } 
+  const handleEditGoal = (id) => {
+    let updatedGoal = {
+      goalValue: editState.goal
+    }
+    axios.put(`/api/categories/${id}`, updatedGoal, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('user')}`
+      }
+    }).then(res => {
+      console.log(res)
+    })
 
-  // } 
+  } 
   const handleInputChange = ({ target: { name, value } }) => {
     setEditState({ ...editState, [name]: value })
   }
-
   return (
     <>
-      {cardState ?
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 12 }} >
-          <Grid item xs={2}><h6 tag>Category: {category}</h6></Grid>
-          <Grid item xs={2}><TextField name="actual" onChange={handleInputChange} defaultValue={editState.actual} label="Actual" ></TextField></Grid>
-          <Grid item xs={2}><TextField name="goal" onChange={handleInputChange} defaultValue={editState.goal} label="Goal"></TextField></Grid>
-          <Grid item xs={2}><span>Result: {editState.result}</span></Grid>
-          <Grid item xs={1}><Button variant="outlined" onClick={() => handleEdit(_id)}>Update</Button></Grid>
-          <Grid item xs={1}><Button variant="outlined" onClick={() => handleRemoveExpense(_id)}>Remove Expense</Button></Grid>
-        </Grid >
-        : null}
-    </>
+    { cardState ? 
+      <Grid container rowSpacing = { 1 } columnSpacing = {{ xs: 12 } } >
+      <Grid item xs={2}><h6 tag>Category: {category}</h6></Grid>
+      <Grid item xs={2}><TextField name= "actual" onChange= {handleInputChange} defaultValue= {actualValue} label= "Actual" ></TextField></Grid>
+      <Grid item xs={2}><TextField name="goal" onChange={handleInputChange} defaultValue={goalValue} label= "Goal"></TextField></Grid>
+      <Grid item xs={2}><span>Result: {result}</span></Grid>
+      <Grid item xs={1}><Button variant="outlined" onClick={() => handleEditActual(_id)}>Update Actual</Button></Grid>
+      <Grid item xs={1}><Button variant="outlined" onClick={() => handleEditGoal(_id)}>Update Goal</Button></Grid>
+      <Grid item xs={1}><Button variant="outlined" onClick = {() => handleRemoveExpense(_id)}>Remove Expense</Button></Grid>
+    </Grid >
+    : null }
+    </> 
   )
 
 
