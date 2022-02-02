44d3b6d5f027967affdeae912c0ab1bf705bea84
timduran <durantimothy21@gmail.com>
2022-02-02 13:08:27 -0800

    upated project

---
+++ client/src/pages/AboutUs/AboutUs 2.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+import NavBar from '../../components/NavBar';
+import ProfileCard from '../../components/AboutUsCard';
+import Footer from '../../components/Footer'
+
+
+const AboutUs = ()=> {
+  return(
+    <>
+      <NavBar></NavBar>
+      <ProfileCard></ProfileCard>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default AboutUs;
