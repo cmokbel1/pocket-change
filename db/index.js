06dd09718474d3254ddc4335fec621c720af38d5
Buff4eyes <claudemokbel@gmail.com>
2022-02-02 17:28:53 -0800

    removed localhost from db

--- db/index.js
+++ db/index.js
@@ -1,1 +1,1 @@
-module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/pocketchange_db' || 'mongodb+srv://ClaudeM:fishsandwich@homeworkstuff.mjead.mongodb.net/pocket_change_db?retryWrites=true&w=majority')
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb+srv://ClaudeM:fishsandwich@homeworkstuff.mjead.mongodb.net/pocket_change_db?retryWrites=true&w=majority')
